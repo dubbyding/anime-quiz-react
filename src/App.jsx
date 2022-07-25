a59ca5e57b2ae6cded6b97518793807e8743c29c
Raj Maharjan <rmaharjan.rm90@gmail.com>
2022-07-25 13:06:25 +0545

    landing page complete with action to another page when on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Landing from './components/landingPage/Landing';
 
 export default function () {
+	const [gameStart, setGameStart] = useState(0);
+
+	function startGame() {
+		setGameStart((prevState) => {
+			if (prevState < 2) {
+				return prevState + 1;
+			} else {
+				return prevState - 1;
+			}
+		});
+	}
+
 	return (
 		<React.Fragment>
-			<Landing />
+			{!gameStart && <Landing startGame={startGame} />}
 		</React.Fragment>
 	);
 }
